23c9c61df30af1ca23addad59cc387ebee22f817
andychung0214 <andy6756@gmail.com>
2023-05-16 11:38:31 +0800

    first commit

---
+++ hello.js
@@ -0,0 +1,5 @@
+function sayHello(person) {
+    return 'Hello, ' + person;
+}
+var user = 'Andy';
+console.log(sayHello(user));
---
+++ hello.ts
@@ -0,0 +1,6 @@
+function sayHello(person: string){
+    return 'Hello, ' + person;
+}
+
+let user = 'Andy';
+console.log(sayHello(user));
